466af06dd0b94b96ecc8f819f4611e07046f72fb
PradeepReddyGunna <gunnapradeepreddy07@gmail.com>
2025-05-30 17:17:20 -0500

    java-microservices-app

---
+++ frontend-ui/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>📦 Java Microservices App</h1>
+      <p>User and Order microservices with React UI</p>
+    </div>
+  );
+}
+export default App;
